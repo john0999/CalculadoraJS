628b8a241e5bed660afc1fa5346c7361621c993a
john0999 <moralesgomezjohnny@gmail.com>
2024-06-07 12:57:17 -0600

    Sumar

---
+++ script.js
@@ -0,0 +1,10 @@
+let num1 = document.getElementById("num1").value;
+let num2 = document.getElementById("num2").value;
+
+let numero1 = parseFloat(num1);
+let numero2 = parseFloat(num2);
+
+function sumar(){
+    let suma = numero1 + numero2;
+    document.getElementById("resultado").textContent = suma;
+}
